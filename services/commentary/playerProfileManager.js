ed10b839d3b73c676d9b7382aa0cb7ea0a97033e
unknown <chatzteam@gmail.com>
2025-08-01 10:44:03 +0000

    tweaked the winner/loser SITUATION algorithm abit

--- services/commentary/playerProfileManager.js
+++ services/commentary/playerProfileManager.js
@@ -192,7 +192,7 @@ class PlayerProfileManager {
     // LOSING detection
       if (bottomQuarter || isLast) {
     const recentlyDropping = metrics.lastThreeRoundsDelta < -10 && relativeDelta < -5;
-    const flatOrWeak = Math.abs(lastThreeRoundsDelta) < 5 || scoreGapToNext > 30;
+    //const flatOrWeak = Math.abs(lastThreeRoundsDelta) < 5 || scoreGapToNext > 30;
 
     return {
       situation: 'LOSING',
